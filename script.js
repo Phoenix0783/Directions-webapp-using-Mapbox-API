29d9be093a91e132f8a4e942fbfac515eb5b2065
Phoenix <nishadajay1099@gmail.com>
2022-10-08 21:39:31 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+mapboxgl.accessToken = 'YOUR MAPBOX API KEY';
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true
+})
+
+function successLocation(position) {
+  setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+function errorLocation() {
+  setupMap([78.5971993,21.9488679])
+}
+
+function setupMap(center) {
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: center,
+    zoom: 15
+  })
+
+  const nav = new mapboxgl.NavigationControl()
+  map.addControl(nav)
+
+  var directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken
+  })
+
+  map.addControl(directions, "top-left")
+}
